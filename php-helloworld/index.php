38e5a9bf4866a042b46ee14c9d8939afc530f2f1
ja007c1 <127996439+ja007c1@users.noreply.github.com>
2023-03-29 14:57:14 -0600

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-printt "Un cambio esta en camino!!! " . PHP_VERSION . "\n";
+print "Un cambio esta en camino!!! " . PHP_VERSION . "\n";
 ?>
